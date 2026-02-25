21b3514cb9ca238b54188d89415e0af8dcfff323
Vikash Prajapati <vikash.prajapati@nagarro.com>
2026-02-25 23:26:24 +0530

    created jwt filter

--- src/main/java/com/spring/jwt/filter/JwtFilter.java
+++ src/main/java/com/spring/jwt/filter/JwtFilter.java
@@ -1,4 +1,16 @@
 package com.spring.jwt.filter;
 
-public class JwtFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
 }
