19eae1facc80be30120b76faaf31626453c77998
Vikash Prajapati <vikash.prajapati@nagarro.com>
2026-03-02 22:31:50 +0530

    implemented authentication through JWT Token

--- src/main/java/com/spring/jwt/configuration/AuthConfiguration.java
+++ src/main/java/com/spring/jwt/configuration/AuthConfiguration.java
@@ -1,27 +1,30 @@
 package com.spring.jwt.configuration;
 
-import com.spring.jwt.service.EngineerUserDetailsService;
+import com.spring.jwt.filter.JwtFilter;
+import com.spring.jwt.service.EngineerCredUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class AuthConfiguration {
 
     @Autowired
-    private EngineerUserDetailsService engineerUserDetailsService;
+    private EngineerCredUserDetailsService engineerCredUserDetailsService;
+
+    @Autowired
+    private JwtFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
@@ -30,15 +33,16 @@ public class AuthConfiguration {
                         .requestMatchers("/register","/login")
                         .permitAll()
                         .anyRequest().authenticated())
-                        .httpBasic(Customizer.withDefaults())
-                        .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                        //.httpBasic(Customizer.withDefaults())
+                        .sessionManagement(session->session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
     }
 
 
     @Bean
     public AuthenticationProvider authenticationProvider(){
-        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider(engineerUserDetailsService);
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider(engineerCredUserDetailsService);
         daoAuthenticationProvider.setPasswordEncoder(new BCryptPasswordEncoder(12));
         //daoAuthenticationProvider.setUserDetailsPasswordService(); //used to automatically update and re-encode user passwords in the database after successful login when password encoding rules change.
         return daoAuthenticationProvider;
--- src/main/java/com/spring/jwt/service/JwtService.java
+++ src/main/java/com/spring/jwt/service/JwtService.java
@@ -1,9 +1,11 @@
 package com.spring.jwt.service;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.KeyGenerator;
@@ -15,6 +17,7 @@ import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -36,24 +39,49 @@ public class JwtService {
         }
     }
 
+    public String generateToken(String eId) {
+        Map<String, Object> claims = new HashMap<>();
+        return Jwts.builder()
+                .claims(claims)
+                .subject(eId)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000*60*3))
+                .signWith(getKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
     private Key getKey(){
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
 
-    public String generateToken(String eId) {
-
-        Map<String, Object> claims = new HashMap<>();
-        return Jwts.builder()
-                .claims(claims)
-                .subject(eId)
-                .issuedAt(new Date(System.currentTimeMillis()))
-                .expiration(new Date(System.currentTimeMillis() + 1000*60*1))
-                .signWith(getKey(), SignatureAlgorithm.HS256)
-                .compact();
+    public String extractEngineerCredId(String token) {
+        return extractClaim(token, Claims::getSubject);
     }
 
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims =  extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().verifyWith((SecretKey) getKey())
+                .build().parseSignedClaims(token).getPayload();
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails) {
+        final String eId = extractEngineerCredId(token);
+        return (eId.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
 
 
 }
